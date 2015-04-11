a198111ef88e9a8bff799b5a0c8f060c32ea027f
Vwing <vwing@uci.edu>
2015-04-10 21:36:22 -0700

    Now Player can move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 1.0f;
+
+	void Start () 
+	{
+
+	}
+
+	void Update ()
+	{
+		Vector3 walkDirection = new Vector3(Input.GetAxis("Horizontal"),0,0);
+		transform.Translate(walkDirection * speed * Time.deltaTime);
+	}
+}
