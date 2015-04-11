8ebdb1345765b2267ccf0ca3ed5da48e8420f827
Vwing <vwing@uci.edu>
2015-04-10 23:58:57 -0700

    Rotates player when changing direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,36 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 	public float speed = 1.0f;
+	bool right = true;
+	float xAxis;
+	Transform sprite;
 
 	void Start () 
 	{
-
+		sprite = transform.FindChild("Sprite");
 	}
 
 	void Update ()
 	{
-		Vector3 walkDirection = new Vector3(Input.GetAxis("Horizontal"),0,0);
+		LateralMovement();
+	}
+
+	void LateralMovement()
+	{
+		xAxis = Input.GetAxis ("Horizontal");
+		if(xAxis > 0 && !right || xAxis < 0 && right){
+			right = !right;
+			sprite.RotateAround(transform.position + Vector3.left * -0.5f, Vector3.up, 180);
+		}
+		Vector3 walkDirection = new Vector3(xAxis,0,0);
 		transform.Translate(walkDirection * speed * Time.deltaTime);
 	}
+//	void Rotate180()
+//	{
+//		sprite.Rotate(Vector3.up * -180);
+//		if(!right)
+//			sprite.localPosition = Vector3.left * -1.0f;
+//		else
+//			sprite.localPosition = Vector3.zero;
+//	}
 }
