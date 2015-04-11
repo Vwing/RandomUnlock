7b1ff1bd296ab1189dadfe378d800514031fe7ed
Vwing <vwing@uci.edu>
2015-04-11 10:26:33 -0700

    Polished Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,26 +3,34 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-	public float speed = 1.0f;
+	public float walkSpeed = 1.0f;
 	public float jumpForce = 450.0f;
 	public float jetForce = 1.0f;
+
 	bool right = true;
+	bool jumpAllowed = false;
+	float feetPos;
+
 	Transform sprite;
 	Rigidbody2D rb;
+	Animator anim;
 
 	void Start () 
 	{
 		sprite = transform.FindChild("Sprite");
 		rb = GetComponent<Rigidbody2D>();
+		anim = sprite.GetComponent<Animator>();
+
+		feetPos = GetComponent<BoxCollider2D>().bounds.extents.y;
 	}
 
 	void Update ()
 	{
 		LateralMovement();
-		if(Input.GetButtonDown ("Jump"))
-			Jump ();
 		if(Input.GetButton ("Fire1"))
 			Jetpack ();
+		if(Input.GetButtonDown ("Jump") && jumpAllowed)
+			Jump ();
 	}
 
 	float xAxis;
@@ -30,12 +38,18 @@ public class Player : MonoBehaviour
 	void LateralMovement()
 	{
 		xAxis = Input.GetAxis ("Horizontal");
+
+		if(xAxis == 0)
+			anim.enabled = false;
+		else
+			anim.enabled = true;
+
 		if(xAxis > 0 && !right || xAxis < 0 && right){
 			right = !right;
 			sprite.RotateAround(transform.position + Vector3.left * -0.5f, Vector3.up, 180);
 		}
 		Vector3 walkDirection = new Vector3(xAxis,0,0);
-		transform.Translate(walkDirection * speed * Time.deltaTime);
+		transform.Translate(walkDirection * walkSpeed * Time.deltaTime);
 	}
 
 	void Jump()
@@ -47,6 +61,25 @@ public class Player : MonoBehaviour
 	{
 		rb.AddForce(Vector3.up * jetForce);
 	}
+
+	void OnCollisionStay2D(Collision2D other)
+	{
+//		string otherTag = other.gameObject.tag;
+//		if(otherTag == "Ground")
+			jumpAllowed = true;
+	}
+
+	void OnCollisionExit2D(Collision2D other)
+	{
+//		string otherTag = other.gameObject.tag;
+//		if(otherTag == "Ground")
+			jumpAllowed = false;
+	}
+	
+//	bool IsGrounded()
+//	{
+//		return Physics2D.Raycast (transform.position,-Vector2.up,feetPos);
+//	}
 
 //	void Rotate180()
 //	{
