063c7172c1837b2fd648c259c34b10490f71d5cd
Vwing <vwing@uci.edu>
2015-04-12 16:54:40 -0700

    Reverted changes to Patrol, made PatrolFlip

--- Assets/Scripts/PatrolFlip.cs
+++ Assets/Scripts/PatrolFlip.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Patrol : MonoBehaviour
+public class PatrolFlip : MonoBehaviour
 {
 	public Vector3 patrolDistance;
 	public float patrolSpeed = 1.0f;
-	
+
 	Vector3 initPos;
 	Vector3 targetPos;
 	Vector3 direction;
 	bool goingTowardsTarget = true;
-	
+	bool firstTime = true;
+
 	void Start ()
 	{
 		initPos = transform.position;
@@ -18,18 +19,35 @@ public class Patrol : MonoBehaviour
 		direction = (targetPos - initPos).normalized;
 	}
 	
-	void Update ()
+	void FixedUpdate ()
 	{
 		if(Vector3.Dot (direction,targetPos - transform.position) <= 0){
 			goingTowardsTarget = false;
+			Flip();
 		} 
 		else if(Vector3.Dot (direction,initPos - transform.position) >= 0){
 			goingTowardsTarget = true;
+			Flip();
 		}
-		
+
 		if(goingTowardsTarget)
 			transform.Translate(direction * patrolSpeed * Time.deltaTime);
 		else
 			transform.Translate(-1.0f * direction * patrolSpeed * Time.deltaTime);
+	}
+
+	void Flip ()
+	{
+		// Multiply the player's x local scale by -1.
+		if(!firstTime)
+		{
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+		else
+		{
+			firstTime = false;
+		}
 	}
 }
