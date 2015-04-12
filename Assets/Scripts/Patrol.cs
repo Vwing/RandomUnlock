a194123074b12ccd0aeb0a408002be363aeb40c3
andresg6 <andresg6@uci.edu>
2015-04-12 16:23:33 -0700

    Changed Patrol Script , requires changes to all enemies -Updated Mountain Yeti & Jungle Monster to match new patrol

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -10,6 +10,7 @@ public class Patrol : MonoBehaviour
 	Vector3 targetPos;
 	Vector3 direction;
 	bool goingTowardsTarget = true;
+	bool right = true;
 
 	void Start ()
 	{
@@ -18,18 +19,36 @@ public class Patrol : MonoBehaviour
 		direction = (targetPos - initPos).normalized;
 	}
 	
-	void Update ()
+	void FixedUpdate ()
 	{
 		if(Vector3.Dot (direction,targetPos - transform.position) <= 0){
 			goingTowardsTarget = false;
+			Flip();
 		} 
 		else if(Vector3.Dot (direction,initPos - transform.position) >= 0){
 			goingTowardsTarget = true;
+			Flip();
 		}
 
 		if(goingTowardsTarget)
 			transform.Translate(direction * patrolSpeed * Time.deltaTime);
 		else
 			transform.Translate(-1.0f * direction * patrolSpeed * Time.deltaTime);
+	}
+
+	void Flip ()
+	{
+		// Switch the way the player is labelled as facing.
+		right = !right;
+
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+
+//		if(right)
+//			transform.position += new Vector3(1, 0, 0);
+//		else
+//			transform.position  -= new Vector3(-1, 0, 0);
 	}
 }
