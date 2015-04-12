d95380c38c4048bd4a264df82ecdebc5f3c9e9e4
Vwing <vwing@uci.edu>
2015-04-12 11:22:55 -0700

    Organized folder structure

---
+++ Assets/Scripts/Jetpack.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jetpack : MonoBehaviour
+{
+	Animator anim;
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	void Update ()
+	{
+		if(Input.GetButton ("Fire1"))
+			anim.Play("JetpackOn");
+		else
+			anim.Play("JetpackOff");
+	}
+}
