0b2a99e41ecc38f0bb1d09ccdbbbdb9d5f486a74
andresg6 <andresg6@uci.edu>
2015-04-11 19:52:55 -0700

    AchievementController now prases achievements from .txt into a formatted list.

--- Assets/Scripts/AchievementController.cs
+++ Assets/Scripts/AchievementController.cs
@@ -4,16 +4,36 @@ using System.Collections.Generic;
 
 public class AchievementController : MonoBehaviour 
 {
+	public string AchievementFilePath = "Text/achievements";
+	public struct Achievement 
+	{
+		public string biome;
+		public string trigger;
+		public string description;
 
-	// Use this for initialization
+		public Achievement(string loc, string trig, string text)
+		{
+			biome = loc;
+			trigger = trig;
+			description = text;
+		}
+	}
+
+	public List<Achievement> AchievementList = new List<Achievement>();
+
+	void LoadAllAchievements()
+	{
+		TextAsset achtxt = Resources.Load (AchievementFilePath) as TextAsset;
+		foreach(string line in achtxt.text.Split(new char[1]{'\n'}))
+		{
+			string[] achArr = line.Split (new char[1]{';'});
+			AchievementList.Add (new Achievement(achArr[0], achArr[1], achArr[2]));
+		}
+	}
+
 	void Start () 
 	{
-	
+		LoadAllAchievements();
 	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
-	}
+
 }
