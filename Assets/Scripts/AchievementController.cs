b7c6630c3d49ce9a08441086018888737804e33d
Vwing <vwing@uci.edu>
2015-04-12 15:51:48 -0700

    AchievementController now chooses 5 random objectives

--- Assets/Scripts/AchievementController.cs
+++ Assets/Scripts/AchievementController.cs
@@ -4,59 +4,94 @@ using System.Collections.Generic;
 
 public class AchievementController : MonoBehaviour 
 {
-//	string AchievementFilePath = "Text/achievements";
-//	List<Achievement> FullAchievementList = new List<Achievement>();
-//	List<Achievement> AchievementList = new List<Achievement>();
-//
-//	public struct Achievement 
-//	{
-//		public string biome;
-//		public string trigger;
-//		public string description;
-//		public bool unlocked = false;
-//
-//		public Achievement(string loc, string trig, string text)
+	string AchievementFilePath = "Text/achievements";
+	List<Achievement> FullAchievementList = new List<Achievement>();
+	[HideInInspector]
+	public List<Achievement> AchievementList = new List<Achievement>();
+	[HideInInspector]
+	public List<Achievement> Objectives = new List<Achievement>();
+
+	public int numAchivementsNeeded = 5;
+
+	public struct Achievement 
+	{
+		public string biome;
+		public string trigger;
+		public string description;
+		public bool unlocked;
+
+		public Achievement(string loc, string trig, string text)
+		{
+			biome = loc;
+			trigger = trig;
+			description = text;
+			unlocked = false;
+		}
+	}
+
+
+	void LoadAllAchievements()
+	{
+		TextAsset achtxt = Resources.Load (AchievementFilePath) as TextAsset;
+		foreach(string line in achtxt.text.Split(new char[1]{'\n'}))
+		{
+			string[] achArr = line.Split (new char[1]{';'});
+			FullAchievementList.Add (new Achievement(achArr[0], achArr[1], achArr[2]));
+		}
+	}
+
+	void LoadObjectives(string biome)
+	{
+		AchievementList.Clear();
+		foreach(Achievement a in FullAchievementList)
+		{
+			if(a.biome == biome || a.biome == "A")
+				AchievementList.Add (a);
+		}
+		GenerateRandomObjectives ();
+	}
+
+	void GenerateRandomObjectives()
+	{
+		Objectives.Clear();
+		Shuffle (AchievementList);
+		for(int i = 0; i < numAchivementsNeeded; ++i)
+			Objectives.Add (AchievementList[i]);
+
+//		int count = AchievementList.Count;
+//		int needed = numAchivementsNeeded - 1;
+//		for(int i = 0; i < count; ++i)
 //		{
-//			biome = loc;
-//			trigger = trig;
-//			description = text;
+//			if(Random.Range (needed, count - i) <= needed){
+//				Objectives.Add (AchievementList[i]);
+//				--needed;
+//			}
 //		}
-//	}
-//
-//
-//	void LoadAllAchievements()
-//	{
-//		TextAsset achtxt = Resources.Load (AchievementFilePath) as TextAsset;
-//		foreach(string line in achtxt.text.Split(new char[1]{'\n'}))
-//		{
-//			string[] achArr = line.Split (new char[1]{';'});
-//			FullAchievementList.Add (new Achievement(achArr[0], achArr[1], achArr[2]));
-//		}
-//	}
-//
-//	void LoadAchievements(string biome)
-//	{
-//		foreach(Achievement a in FullAchievementList)
-//		{
-//			if(a.biome == biome || a.biome == "A")
-//				AchievementList.Add (a);
-//		}
-//	}
-//
-//	void ClearAchievements()
-//	{
-//		AchievementList.Clear();
-//	}
-//
-//	void Awake()
-//	{
-//		LoadAllAchievements ();
-//	}
-//
-//	void Start () 
-//	{
-//		LoadAllAchievements();
-//	}
+	}
+
+	void Shuffle(List<Achievement> list) {
+		int n = list.Count;
+		//Random rnd = new Random();
+		while (n > 1) {
+			int k = (Random.Range(0, n) % n);
+			n--;
+			Achievement value = list[k];
+			list[k] = list[n];
+			list[n] = value;
+		}
+	}
+	
+	void Awake()
+	{
+		LoadAllAchievements ();
+	}
+	
+	void Start () 
+	{
+		LoadObjectives("M");
+		foreach(Achievement o in Objectives)
+			Debug.Log (o.description);
+	}
 
 	void Update()
 	{
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,18 +18,18 @@ public class Player : MonoBehaviour
 	float startJumpY;
 	
 	Transform sprite;
-	Transform jetpack;
+//	Transform jetpack;
 	Rigidbody2D rb;
 	Animator anim;
-	BoxCollider2D coll;
+//	BoxCollider2D coll;
 	
 	void Awake()
 	{
 		sprite = transform.FindChild("Sprite");
-		jetpack = transform.FindChild("Jetpack");
+//		jetpack = transform.FindChild("Jetpack");
 		rb = GetComponent<Rigidbody2D>();
 		anim = sprite.GetComponent<Animator>();
-		coll = GetComponent<BoxCollider2D>();
+//		coll = GetComponent<BoxCollider2D>();
 	}
 	
 	void Start () 
@@ -61,12 +61,9 @@ public class Player : MonoBehaviour
 		else
 			anim.enabled = true;
 		
-		if(xAxis > 0 && !right || xAxis < 0 && right){
-			//right = !right;
+		if(xAxis > 0 && !right || xAxis < 0 && right)
 			Flip ();
-//			sprite.RotateAround(transform.position + Vector3.left * -0.5f, Vector3.up, 180);
-//			sprite.RotateAround(transform.position + Vector3.left * -0.5f, Vector3.up, 180);
-		}
+
 		Vector3 walkDirection = new Vector3(xAxis,0,0);
 		transform.Translate(walkDirection * walkSpeed * Time.deltaTime);
 	}
