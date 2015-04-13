22f71418fd3cb83eff5428ffb9853c514e85fa5a
andresg6 <andresg6@uci.edu>
2015-04-12 19:01:57 -0700

    Example of using Regex Expressions in C#

---
+++ Assets/Scripts/Test/RegexTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class RegexTest : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		TestRegex();
+	}
+	
+	void TestRegex()
+	{
+		string pattern = @"(^[a-zA-z]+)([\d]*)";
+		string input = "Jammin456";
+
+		Match match = Regex.Match(input, pattern);
+
+		Debug.Log (match.Groups[1] + " " + match.Groups[2] + " " + match.Groups[3]);
+
+	}
+}
